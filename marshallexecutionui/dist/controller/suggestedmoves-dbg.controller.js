ac71cc325765461ed694b9c384189eeae906a12b
Jacob Ellwood <jacob.ellwood@bluestonex.com>
2026-03-18 00:55:51 +0000

    Fixed no refresh on confirm move

--- marshallexecutionui/dist/controller/suggestedmoves-dbg.controller.js
+++ marshallexecutionui/dist/controller/suggestedmoves-dbg.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/BusyIndicator",
+    "sap/m/MessageToast",
+], (Controller, BusyIndicator, MessageToast) => {
     "use strict";
 
     return Controller.extend("com.sysco.wm.marshallexecutionui.marshallexecutionui.controller.suggestedmoves", {
@@ -11,16 +13,85 @@ sap.ui.define([
             var oRouter = this.getOwnerComponent().getRouter();
             oRouter.getRoute("suggestedmoves").attachPatternMatched(this._onRouteMatched, this);
             this.oBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+            
         },
 
         _onRouteMatched: function () {
+
+            this.getPlantData();
 
             this._pageSize = 5;
             this._currentPage = 0;
             this._currentIndex = 0;
 
-            this.suggestedMovesList();
+            
 
+        },
+
+        getPlantData: function () {
+            BusyIndicator.show(500);
+            var appId = this.getOwnerComponent().getManifestEntry("/sap.app/id");
+            var appPath = appId.replaceAll(".", "/");
+            this.appModulePath = jQuery.sap.getModulePath(appPath);
+            var that = this;
+            var oLocale = sap.ui.getCore().getConfiguration().getLocale();
+            var lang = oLocale.language;
+
+            $.ajax({
+                url: this.appModulePath + "/marshallingservices/CloudWM/getPlantListForUser()",
+                beforeSend: function (xhr) { xhr.setRequestHeader('Accept-Language', lang); },
+                type: "GET",
+                contentType: "application/json",
+                dataType: "json",
+                async: true,
+                success: function (oData, response) {
+                    BusyIndicator.hide();
+                    const assignedPlants = oData.value;
+                    assignedPlants.forEach(function (assignedPlant, index) {
+                        if (assignedPlant.DefaultPlant)
+                            that.plant = assignedPlant.Plant;
+                    });
+
+                    that.getMarshallingData(that.plant);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    BusyIndicator.hide();
+                    MessageToast.show(jqXHR.responseText); //, "ERROR", "Service call error");
+                }
+            }, this);
+        },
+
+        getMarshallingData: function (plant) {
+            var appId = this.getOwnerComponent().getManifestEntry("/sap.app/id");
+            var appPath = appId.replaceAll(".", "/");
+            this.appModulePath = jQuery.sap.getModulePath(appPath);
+            var oLocale = sap.ui.getCore().getConfiguration().getLocale();
+            var currentDateUTC = new Date().toISOString().split('T')[0];
+            var dateAfterAWeek = new Date();
+            dateAfterAWeek.setDate(dateAfterAWeek.getDate() + 7);
+            var toDate = dateAfterAWeek.toISOString().split('T')[0];
+            var lang = oLocale.language;
+            var that = this;
+            var sDest = "/marshallingservices";
+            var path = "/Marshalling/PickTaskHeaders?$filter=Plant eq '" + plant + "' and DeliveryDate ge '" + currentDateUTC + "' and DeliveryDate le '" + toDate + "' and To_Marshalling/ID ne null and To_Marshalling/Status_ID ne 'PALLETISED' and To_Marshalling/Status_ID ne 'LOADED' and Status_ID eq 'COMPLETED'&$select=ID,Route,PickJob&$expand=To_Marshalling($expand=MarshallingBinID($select=ID,Description),SuggestedBinID($select=ID,Description)),Status,Media";
+            var sUrl = this.appModulePath + sDest + path;
+            
+            $.ajax({
+                url: sUrl,
+                beforeSend: function (xhr) { xhr.setRequestHeader('Accept-Language', lang); },
+                method: "GET",
+                headers: {
+                    "Accept": "application/json"
+                },
+                success: function (data) {
+                    var oSuggestedMoveData = new sap.ui.model.json.JSONModel({ data });
+                    that.getOwnerComponent().setModel(oSuggestedMoveData, "SuggestedMoveData");
+                    that.suggestedMovesList();
+                },
+                error: function (textStatus, errorThrown) {
+                    console.log("Error:", textStatus, errorThrown);
+                }
+            });
         },
 
         suggestedMovesList: function () {
